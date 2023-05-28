d85ec76569af51d4b0551afcd70b2b390ef52eda
FrankLaquise <a20175947@pucp.edu.pe>
2023-05-28 11:58:00 -0500

    Agre.botones en xml

--- app/src/main/java/com/example/lab04_aiot/MainActivity.java
+++ app/src/main/java/com/example/lab04_aiot/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab04_aiot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void tutor(View view){
+        Intent i = new Intent(this,FlujoTutor.class);
+        startActivity(i);
+
+    }
+    public void trabajador(View view){
+        Intent i = new Intent(this,FlujoTrabajador.class);
+        startActivity(i);
+
+    }
+
 }
