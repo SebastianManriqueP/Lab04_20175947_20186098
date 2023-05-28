021ee5916bedd3f5be7d5b9277a840b95212e058
FrankLaquise <a20175947@pucp.edu.pe>
2023-05-28 12:49:23 -0500

    notificacion tutor

--- app/src/main/java/com/example/lab04_aiot/MainActivity.java
+++ app/src/main/java/com/example/lab04_aiot/MainActivity.java
@@ -1,27 +1,67 @@
 package com.example.lab04_aiot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "tutor_channel";
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        Button tutorButton = findViewById(R.id.tutorButton);
+        tutorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTutorNotification();
+                Intent intent=new Intent(MainActivity.this,FlujoTutor.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void tutor(View view){
-        Intent i = new Intent(this,FlujoTutor.class);
+
+
+
+    public void trabajador(View view) {
+        Intent i = new Intent(this, FlujoTrabajador.class);
         startActivity(i);
-
-    }
-    public void trabajador(View view){
-        Intent i = new Intent(this,FlujoTrabajador.class);
-        startActivity(i);
-
     }
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Tutor Channel";
+            String description = "Channel for tutor notifications";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showTutorNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("Está entrando en modo Tutor")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 }
